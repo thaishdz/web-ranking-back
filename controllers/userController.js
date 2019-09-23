46507d4523ce47b06b3760bc14836f85403395d4
NassrEML <nassr906@gmail.com>
2019-09-23 20:46:11 +0100

    fiz search by name

--- controllers/userController.js
+++ controllers/userController.js
@@ -33,13 +33,13 @@ function getUsersByAffiliate(req, res) {
 function getUsersByName(req,res) {
     const { name }  = req.query;
     
-    let offset = Number(req.query.offset) || 0
+    let offset = Number(req.query.offset) || 0;
     Users.find({
         "name": { "$regex": name, "$options": "i"}})
         .skip(offset * 10).limit(10)
     .select("-NIF -address -zip_code -phone_number -phone_number2 -email -license_inscription_date -expiration_date -price")
         .then(dataset => {
-            Users.countDocuments().then((count) => {
+            Users.countDocuments({"name": { "$regex": name, "$options": "i"}}).then((count) => {
                 res.send({
                     total: count,
                     res: dataset
